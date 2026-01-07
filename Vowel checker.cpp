4df86d45ec1692dd8c288824f86b5e7514afda19
Nimish Bordiya <nimishbordiyajain@gmail.com>
2026-01-07 14:22:03 +0530

    Files of 7Jan

---
+++ Vowel checker.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cout<<"Enter any Character";
+    cin>>ch;
+    
+    switch(ch){
+        case'a':case'e':case'i':case'o':case'u':
+        case'A':case'E':case'I':case'O':case'U':
+         cout<<"You entered"<<ch<< "It is a Vowel.";
+         break;
+        default:
+        cout<<"You entered"<<ch<<".It is NOT a VOWEL."<<endl;
+    return 0;
+    }
+}
---
+++ even numbers till 50.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int ev;
+    ev=1;
+    while(ev<50){
+        if(ev%2 == 0)
+        cout<<ev<<" ";
+        ev++;
+        
+    }
+    return 0;
+}
